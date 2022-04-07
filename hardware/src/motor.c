a4309bcf27fb4ad82390fa17e7a3b704e81c2b50
helloworld654 <zhai.shiqiang@163.com>
2022-04-07 22:40:51 +0800

    [hardware] optimize pwm function using bsp_tim_pwm

--- hardware/src/motor.c
+++ hardware/src/motor.c
@@ -1,44 +1,7 @@
 #include "motor.h"
 #include "stm32f4xx.h"
 
-void TIM9_CH2_PWM_Init(u16 arr,u16 psc)
-{		
-	GPIO_InitTypeDef GPIO_InitStructure;
-	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-	TIM_OCInitTypeDef  TIM_OCInitStructure;
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM9,ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
- 
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_TIM9);
-
-	TIM_TimeBaseStructure.TIM_Prescaler=psc;
-	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;
-	TIM_TimeBaseStructure.TIM_Period=arr;
-	TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1; 
-	TIM_TimeBaseInit(TIM9,&TIM_TimeBaseStructure);
-
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
- 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM_OCInitStructure.TIM_Pulse=0;
-	TIM_OC2Init(TIM9, &TIM_OCInitStructure);
-
-	TIM_OC2PreloadConfig(TIM9, TIM_OCPreload_Enable);
- 
-    TIM_ARRPreloadConfig(TIM9,ENABLE);
-	TIM_Cmd(TIM9, ENABLE);	
-} 
-
 void pwm_func_test(void)
 {
-	TIM9_CH2_PWM_Init(840,1);
-    TIM_SetCompare2(TIM9,200);
+	bsp_SetTIMOutPWM(GPIOA,GPIO_Pin_3,TIM9,2,100000,4000);
 }
